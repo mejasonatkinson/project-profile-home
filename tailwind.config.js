21d77983230e07542c3884b0b72ae6d0f020ed81
mejasonatkinson <me@jasonatkinson.co.uk>
2024-12-13 21:46:49 +0000

    Use colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-off-white": "var(--color-off-white)",
+        "color-off-black": "var(--color-off-black)",
+        "color-yellow": "var(--color-yellow)",
+        "color-red": "var(--color-red)",
+        "color-teal": "var(--color-teal)",
+        "color-dark-off-white": "var(--color-dark-off-white)",
+        "color-dark-off-black": "var(--color-dark-off-black)",
+        "color-dark-yellow": "var(--color-dark-yellow)",
+        "color-dark-red": "var(--color-dark-red)",
+        "color-dark-teal": "var(--color-dark-teal)"
+      }
+    },
   },
   plugins: [],
 }
